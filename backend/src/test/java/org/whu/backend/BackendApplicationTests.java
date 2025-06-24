f55c5e79c40ed343f82058932835b698efbbb3ef
raitocc <raitocc@qq.com>
2025-06-24 12:08:06 +0800

    增加模拟已经支付的接口设计

--- backend/src/test/java/org/whu/backend/BackendApplicationTests.java
+++ backend/src/test/java/org/whu/backend/BackendApplicationTests.java
@@ -3,17 +3,11 @@ package org.whu.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.whu.backend.entity.Spot;
 import org.whu.backend.repository.SpotRepository;
 
 @SpringBootTest
 class BackendApplicationTests {
-
-    @Autowired
-    SpotRepository spotRepository;
-
     @Test
     void contextLoads() {
     }
-
 }
