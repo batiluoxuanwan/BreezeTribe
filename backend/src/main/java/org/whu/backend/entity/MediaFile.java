a0c51b3c25d2049dc1be0eb4fcfc001dca1b5fb3
raitocc <raitocc@qq.com>
2025-06-24 18:52:03 +0800

    准备尝试改动数据库，使其更加易读

--- backend/src/main/java/org/whu/backend/entity/MediaFile.java
+++ backend/src/main/java/org/whu/backend/entity/MediaFile.java
@@ -7,7 +7,6 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.SoftDelete;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.whu.backend.entity.Accounts.Account;
-import org.whu.backend.entity.Accounts.User;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
