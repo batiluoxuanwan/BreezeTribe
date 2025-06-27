19fed168cce541034ed4a50f5edf8396285458df
raitocc <raitocc@qq.com>
2025-06-27 10:47:39 +0800

    注释所有的Friend相关

--- backend/src/main/java/org/whu/backend/repository/authRepo/friend/FriendShipRepository.java
+++ backend/src/main/java/org/whu/backend/repository/authRepo/friend/FriendShipRepository.java
@@ -1,11 +1,11 @@
-package org.whu.backend.repository.authRepo.friend;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.whu.backend.entity.association.friend.Friendship;
-
-public interface FriendShipRepository extends JpaRepository<Friendship, String> {
-    void deleteByAccountIdAndFriendId(String accountId, String friendId);
-    Page<Friendship> findByAccount1(String accountId, Pageable pageable);
-}
+//package org.whu.backend.repository.authRepo.friend;
+//
+//import org.springframework.data.domain.Page;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.whu.backend.entity.association.friend.Friendship;
+//
+//public interface FriendShipRepository extends JpaRepository<Friendship, String> {
+//    void deleteByAccountIdAndFriendId(String accountId, String friendId);
+//    Page<Friendship> findByAccount1(String accountId, Pageable pageable);
+//}
