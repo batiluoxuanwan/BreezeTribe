923801464becb2e80ec30cc1fef0916185847b11
raitocc <raitocc@qq.com>
2025-06-30 17:09:44 +0800

    fix: 关闭烦人的redis自动扫描，减少启动时的大量日志输出

--- backend/src/main/java/org/whu/backend/BackendApplication.java
+++ backend/src/main/java/org/whu/backend/BackendApplication.java
@@ -3,11 +3,15 @@ package org.whu.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableScheduling // 执行定时任务
+// 明确告诉 Spring Data JPA，你只用管 'org.whu.backend.repository.jpa_repo' 这个包下的仓库
+@EnableJpaRepositories(basePackages = "org.whu.backend.repository")
 public class BackendApplication {
 
     public static void main(String[] args) {
