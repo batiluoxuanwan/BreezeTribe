afa8cc15549fde4f72624f6cb6dfbb0c3e13219b
33-ruby <2921354564@qq.com>
2025-06-30 15:50:21 +0800

    点赞功能

--- vue/src/utils/request.js
+++ vue/src/utils/request.js
@@ -17,7 +17,8 @@ publicAxios.interceptors.request.use(config => {
 
 // 创建带 token 的 Axios 实例：用于需要认证的业务接口
 export const authAxios = axios.create({
-  baseURL: 'https://frp-dad.com:36680/api',
+  //baseURL: 'https://frp-dad.com:36680/api',
+  baseURL: 'https://121.43.136.251:8080/api',
   headers: {
     'Content-Type': 'application/json'
   }
