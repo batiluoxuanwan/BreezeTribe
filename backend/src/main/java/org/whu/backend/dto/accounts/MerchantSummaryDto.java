4b1a952ad635e15c5e244a551527098e104fb456
ZSY <14634975+gleedean@user.noreply.gitee.com>
2025-07-08 09:14:19 +0800

    注册商户

--- backend/src/main/java/org/whu/backend/dto/accounts/MerchantSummaryDto.java
+++ backend/src/main/java/org/whu/backend/dto/accounts/MerchantSummaryDto.java
@@ -6,11 +6,14 @@ import lombok.Data;
 public class MerchantSummaryDto {
     private String id;
     private String name;
+//    //执照
+//    private String businessLicenseUrl;
+//    //营业许可证
+//    private String businessPermitUrl;
+//    //身份证(正反两面)
+//    private String idCardUrl1;
+//    private String idCardUrl2;
     //执照
-    private String businessLicenseUrl;
-    //营业许可证
-    private String businessPermitUrl;
-    //身份证(正反两面)
-    private String idCardUrl1;
-    private String idCardUrl2;
+    private String businessLicense;
+    private String companyName;
 }
